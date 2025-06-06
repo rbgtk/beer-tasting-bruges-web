29f18be88f3019770163882220a056d4f19178a2
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-06 18:20:15 +0200

    showing maps

---
+++ src/components/guest/LocationCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="p-4 flex flex-col justify-between">
+    <div class="mb-2">
+      <h3 class="text-lg font-semibold mb-2">{{ location.name }}</h3>
+      <p class="text-sm text-gray-600"><span class="font-semibold">Address:</span> {{ location.address }}</p>
+      <p class="text-sm text-gray-600"><span class="font-semibold">Description:</span> {{ location.description }}</p>
+    </div>
+    <div v-html="location.mapUrl"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Location } from '@/models/location'
+
+defineProps<{ location: Location }>()
+</script>
