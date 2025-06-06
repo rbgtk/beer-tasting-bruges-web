b7a86cc6662259f336e4e58ca6f0847fcfcfc4ee
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-06 13:37:53 +0200

    guest components

---
+++ src/components/guest/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/posts">Articles</router-link>
+    <router-link to="/reservations">Reservations</router-link>
+  </nav>
+</template>
+
