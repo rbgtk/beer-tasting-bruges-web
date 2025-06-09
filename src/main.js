e2d27a04d465ef2d81741ae5a136533545f4f06d
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-09 19:03:34 +0200

    init authStore

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import './style.css'
 import '@vuepic/vue-datepicker/dist/main.css'
 import App from './App.vue'
 import router from '@/router'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.mount('#app')
