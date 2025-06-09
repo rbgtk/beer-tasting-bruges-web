f49b364f3dbdda27e4f68568ca07a5809a5b2cdb
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-06-09 16:14:33 +0200

    announcement service layer

--- src/services/announcementService.js
+++ src/services/announcementService.js
@@ -1,6 +1,25 @@
 import api from '@/axios'
 
+export async function createAnnouncement(announcement) {
+  const response = await api.post('/api/announcements', announcement)
+  return response.data
+}
+
 export async function fetchAnnouncements() {
   const response = await api.get('/api/announcements')
   return response.data
+}
+
+export async function fetchAnnouncement(id) {
+  const response = await api.get(`/api/announcements/${id}`)
+  return response.data
+}
+
+export async function updateAnnouncement(announcement) {
+  const response = await api.put(`/api/announcements/${announcement.id}`, announcement)
+  return response.data
+}
+
+export async function deleteAnnouncement(announcement) {
+  await api.delete(`/api/announcements/${announcement.id}`)
 }
