92c1fe7cda834ef1941e6deca99a2aa0fb906e6c
rbgtk <28433135+rbgtk@users.noreply.github.com>
2025-07-04 16:00:16 +0200

    Showing stats

--- src/pages/admin/Dashboard.vue
+++ src/pages/admin/Dashboard.vue
@@ -1,1 +1,21 @@
-<template>This is a placeholder for now</template>
+<template>
+  <div>You have {{ announcementStore.items.length }} announcements.</div>
+  <div>You have {{ locationStore.items.length }} locations.</div>
+  <div>You have {{ serviceStore.items.length }} services.</div>
+  <div>You have {{ unavailabilityStore.items.length }} unavailabilities.</div>
+  <div>You have {{ bookingStore.items.length }} bookings.</div>
+</template>
+
+<script setup>
+import { useAnnouncementStore } from '@/stores/announcementStore.js'
+import { useLocationStore } from '@/stores/locationStore'
+import { useServiceStore } from '@/stores/serviceStore.js'
+import { useUnavailabilityStore } from '@/stores/unavailabilityStore.js'
+import { useBookingStore } from '@/stores/bookingStore.js'
+
+const announcementStore = useAnnouncementStore()
+const locationStore = useLocationStore()
+const serviceStore = useServiceStore()
+const unavailabilityStore = useUnavailabilityStore()
+const bookingStore = useBookingStore()
+</script>
